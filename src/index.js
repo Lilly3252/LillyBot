9d81e4c65c76be4ce78b12b2bdb82062b665c772
Lilly3252 <74218901+Lilly3252@users.noreply.github.com>
2020-12-01 17:41:40 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const client = new LillyClient(config);
 const discordUNO = new DiscordUNO();
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://Lilly-dev:V0TFU0jowTxpIvyd@lillybot.43rtj.mongodb.net/Lilly',{
+mongoose.connect('LINK HERE',{
 useNewUrlParser: true ,useUnifiedTopology: true});
 console.log('im connected to Database!');
 
