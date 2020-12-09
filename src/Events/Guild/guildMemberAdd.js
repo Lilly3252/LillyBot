2e9e8890d2ddf0d913a3f7baa8026ea65567fd06
Lauriane Veilleux <veilleux_lory@live.ca>
2020-12-09 09:48:57 -0500

    Delete discord-uno

--- src/Events/Guild/guildMemberAdd.js
+++ src/Events/Guild/guildMemberAdd.js
@@ -1,7 +1,6 @@
 const Event = require("../../Structures/Event.js");
 const { MessageEmbed } = require("discord.js");
-const Guild = require("../../Database/models/Guild");
-const moment = require("moment");
+const Guild = require("../../Database/models/Guild.js");
 module.exports = class extends (
   Event
 ) {
@@ -13,47 +12,16 @@ module.exports = class extends (
 
   async run(member) {
     if (!member.guild) return;
-    const settings = await Guild.findOne({
-        guildID: member.guild.id,
-      });
-    function colorFromDuration(duration = Number) {
-      const percent = Math.min(duration / (MAX_TRUST_ACCOUNT_AGE / 100), 100);
-      let r;
-      let g;
-      let b = 0;
-
-      if (percent < 50) {
-        r = 255;
-        g = Math.round(5.1 * percent);
-      } else {
-        g = 255;
-        r = Math.round(510 - 5.1 * percent);
-      }
-
-      const tintFactor = 0.3;
-
-      r += (255 - r) * tintFactor;
-      g += (255 - g) * tintFactor;
-      b += (255 - b) * tintFactor;
-
-      return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
-    }
-    const sinceCreationFormatted = moment
-      .utc(member.user.createdAt ?? 0)
-      .fromNow();
-    const creationFormatted = moment
-      .utc(member.user.createdAt ?? 0)
-      .format(DATE_FORMAT_WITH_SECONDS);
-    const joinFormatted = moment.utc().format(DATE_FORMAT_WITH_SECONDS);
+      const WelcomeChannel = await Guild.findOne({
+        guildID: member.guild.id
+      })
+      if(WelcomeChannel.PersonalizedWelcomeMessage === undefined){return}
     const embed = new MessageEmbed()
-      .setColor(colorFromDuration(Date.now() - member.user.createdTimestamp))
-      .setAuthor(
-        `${member.user.tag} (${member.id})`,
-        member.user.displayAvatarURL()
-      )
+      .setColor("RANDOM")
+      .addField(settings.PersonalizedWelcomeMessage)
       .setFooter("User joined")
       .setTimestamp(new Date());
-      const channelLogs = member.guild.channels.cache.get(settings.logchannelID);
+      const channelLogs = member.guild.channels.cache.get(welcomeChannel.welcomechannelID);
     if (!channelLogs) return;
     channelLogs.send(embed);
   }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const LillyClient = require("./Structures/LillyClient");
 const config = require("./config.json");
-const { DiscordUNO } = require("discord-uno");
+
 const client = new LillyClient(config);
-const discordUNO = new DiscordUNO();
+
 const mongoose = require('mongoose');
 
-mongoose.connect('LINK',{
+mongoose.connect('mongodb+srv://Lilly-dev:V0TFU0jowTxpIvyd@lillybot.43rtj.mongodb.net/Lilly',{
 useNewUrlParser: true ,useUnifiedTopology: true});
 console.log('im connected to Database!');
 
